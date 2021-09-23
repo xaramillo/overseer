3a8c34ba51e87a2717934614e3c1b3f0d1e8177c
xaramillo <luis.jaramillo@solariabiodata.com.mx>
2021-09-22 23:43:38 -0500

    "anotaciones"

--- conda_sourcing.sh
+++ conda_sourcing.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # @xaramillo
 #
-# Este es el script front
+# Script para sourcear
 #
 #
-# Paso 1: Figurando dónde se encuentra instalado Miniconda
-# CONDA_EXE es una variable predefinida en la instalación de Conda
 # La necesidad de este script es porque el ejecutable de conda no está habilitado para ejecución no-interactiva
 # En la primera ejecución de este script, seguramente no exista la siguiente variable de entorno
 
@@ -17,6 +15,8 @@ get_conda_installation_path() {
     if [ -z $CONDA_INSTALL_PATH ]; then
       echo "Conda Path: $CONDA_INSTALL_PATH"
     else
+      # Figurando dónde se encuentra instalado Miniconda
+      # CONDA_EXE es una variable predefinida en la instalación de Conda
       CONDA_INSTALL_PATH=$(echo $CONDA_EXE | sed 's/bin\/conda//')
       echo -e "\n# Línea Agregada por $(whoami), $0 $(date)" >> ~/.bashrc
       echo -e "export CONDA_INSTALL_PATH=$CONDA_INSTALL_PATH\n" >>  ~/.bashrc
@@ -26,9 +26,14 @@ get_conda_installation_path() {
 }
 
 get_conda_installation_path
+# Este es el archivo que buscamos sourcear para poder ejecutar Conda dentro de cualquier script
 CONDA_SH=$(echo "$CONDA_INSTALL_PATH/etc/profile.d/conda.sh" | sed 's/\t//')
 if [ -f $CONDA_SH ]; then
   echo "Sourceando ... $CONDA_SH"
   source $CONDA_SH
 else
   echo "No se encontró el archivo $CONDA_SH"
+
+
+# No encuentro posibles causas de porqué no se podria encontrar el archivo en esta ruta, más que compatiblidad
+# de versiones menores a Miniconda 3
